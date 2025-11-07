7241fb3c8a4ff7c9f7b26137d7a8efe34a430b1b
Theon Antwan Beckford <t.a.beckford@se21.qmul.ac.uk>
2025-11-07 13:49:19 +0000

    Factorial Calculator code

---
+++ FCalc.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class FCalc {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the factorial calculator");
+        factorialCalc();
+    }
+
+    public static void factorialCalc() {
+
+        System.out.println("What number would you like to find the factorial of? ");
+
+        int userNum = 0;
+
+        try {
+            userNum = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("Closing as integer not inputted.");
+            return;
+        }
+
+        float result = recursiveLoop(userNum);
+
+        System.out.println("The factorial of " + userNum + " is " + result);
+
+    }
+
+    public static float recursiveLoop(int num) {
+        if (num == 1) return num;
+        return num * recursiveLoop(num - 1);
+    }
+
+}
